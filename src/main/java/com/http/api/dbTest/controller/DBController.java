2d5c3d8b9b82006107b1bdd3f81af22ddfe1c8ac
Srinath <shankarsrinath@gmail.com>
2023-03-17 15:55:47 +0530

    Refactor : sk - log4j dependency add and logging

--- src/main/java/com/http/api/dbTest/controller/DBController.java
+++ src/main/java/com/http/api/dbTest/controller/DBController.java
@@ -1,5 +1,8 @@
 package com.http.api.dbTest.controller;
 
+import javafx.application.Application;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +17,17 @@ public class DBController {
     @Autowired
     DataSource dataSource;
 
+    private static final Logger LOGGER = LogManager.getLogger(Application.class);
+
     @RequestMapping("/live")
     public String checkDBStatus (){
         try {
+            LOGGER.info("Database Connection establish...");
             Connection connection = dataSource.getConnection();
+            LOGGER.info("Database Connection established.");
             return "Well done";
         } catch (SQLException e) {
+            LOGGER.debug("Database Connection issue...");
             return "Maintenance";
         }
 

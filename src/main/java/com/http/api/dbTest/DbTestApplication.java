f3e4dd9b426bfbfdd1c5c006bf2178f2661c0f2d
Srinath <shankarsrinath@gmail.com>
2023-03-17 15:42:31 +0530

    initial commit with spring dependencies

---
+++ src/main/java/com/http/api/dbTest/DbTestApplication.java
@@ -0,0 +1,13 @@
+package com.http.api.dbTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/http/api/dbTest/DbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.http.api.dbTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

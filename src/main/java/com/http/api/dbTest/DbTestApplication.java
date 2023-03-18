f14289cdd84a013e4d35c45ea4e738ca289b64b1
Srinath <shankarsrinath@gmail.com>
2023-03-18 15:05:14 +0530

    Refactor : sk -javafx package issue fix

--- src/main/java/com/http/api/dbTest/DbTestApplication.java
+++ src/main/java/com/http/api/dbTest/DbTestApplication.java
@@ -1,17 +1,11 @@
 package com.http.api.dbTest;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class DbTestApplication {
-
-	private static final Logger LOGGER = LogManager.getLogger(Application.class);
-	public static void main(String[] args) {
-		LOGGER.info("Application Started ...");
+		public static void main(String[] args) {
 		SpringApplication.run(DbTestApplication.class, args);
 	}
 

c8857b0df8d6cbf9029b6a193626601d14520463
Srinath <shankarsrinath@gmail.com>
2023-03-18 16:25:54 +0530

    Refactor : sk -unused javafx package rmv

--- src/main/java/com/http/api/dbTest/controller/DBController.java
+++ src/main/java/com/http/api/dbTest/controller/DBController.java
@@ -1,6 +1,5 @@
 package com.http.api.dbTest.controller;
 
-import javafx.application.Application;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
